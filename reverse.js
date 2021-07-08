8ede0c488e76571495f30877ca9fb00c3b629506
syd333 <sydmiranda@icloud.com>
2021-07-08 12:09:45 -0400

    reverse recursion

---
+++ reverse.js
@@ -0,0 +1,10 @@
+function reverse(str){
+    if (str.lengt === 0) return str;
+    return reverse(str.slice(1)) + str[0];
+    
+  }
+  
+  // reverse('awesome') // 'emosewa'
+  // reverse('rithmschool') // 'loohcsmhtir'
+
+  //write a recursive function which accepts a string and returns a new string in reverse
