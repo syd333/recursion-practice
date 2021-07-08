d4805fda467148fc67a8d063aafe7ee40cea0345
syd333 <sydmiranda@icloud.com>
2021-07-08 15:37:35 -0400

    recursive callback

---
+++ someRecursive.js
@@ -0,0 +1,17 @@
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(array, callback) {
+    if (array.length === 0) return false;
+    if (callback(array[0])) return true;
+    return someRecursive(array.slice(1),callback);
+}
+
+  // write a function which accepts an array and a callback
+  // function returns true if a single value in the array returns true when passed to the callback
+  // otherwise it returns false
