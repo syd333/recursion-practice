dbacd05d4a95dc6acc5c7fe7971a73eac8f7409d
syd333 <sydmiranda@icloud.com>
2021-07-11 19:19:12 -0400

    finished capitalizeFirst

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,6 +1,11 @@
 function capitalizeFirst (arr) {
-    if (arr.length === 0) return arr
-    
+    if (arr.length === 1) {
+        return [arr[0].toUpperCase()]
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length-1)[0].toUpperCase());
+    return res;
+
   }
   
   // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
