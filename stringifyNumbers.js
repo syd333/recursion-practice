5ee3d949970b58ee9323782ca3d388ccbc44cc3e
syd333 <sydmiranda@icloud.com>
2021-07-11 19:25:18 -0400

    stringify Numbers

---
+++ stringifyNumbers.js
@@ -0,0 +1,49 @@
+
+// write a function which takes in an obj
+// and finds all of the values which are numbers
+// and converts them to strings
+
+
+function stringifyNumbers(obj) {
+    let newObj = {};
+    for (var key in obj) {
+      if (typeof obj[key] === 'number') {
+        newObj[key] = obj[key].toString();
+      } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+        newObj[key] = stringifyNumbers(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  }
+
+/*
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+/*
+
+stringifyNumbers(obj)
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
