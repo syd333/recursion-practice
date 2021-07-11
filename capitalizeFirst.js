6fdb8ab2a8d9cc466045bda933e07d230ad2165f
syd333 <sydmiranda@icloud.com>
2021-07-11 19:16:36 -0400

    capitalize first recursion function

---
+++ capitalizeFirst.js
@@ -0,0 +1,9 @@
+function capitalizeFirst (arr) {
+    if (arr.length === 0) return arr
+    
+  }
+  
+  // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+
+  // given an array of strings
+  // capitalize the first letter of each string in the array
