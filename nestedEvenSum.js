f0deddc0e7c0541a09d2130e311c8b02c42e3701
syd333 <sydmiranda@icloud.com>
2021-07-11 19:21:14 -0400

    nestedEvenSum

---
+++ nestedEvenSum.js
@@ -0,0 +1,36 @@
+function nestedEvenSum (obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
+  }
+  
+  
+  let obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+  
+  let obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+  };
+  
+  nestedEvenSum(obj1); // 6
+  nestedEvenSum(obj2); // 10
+
+  // return the sum of all even numbers in an object which may contain nested objects
